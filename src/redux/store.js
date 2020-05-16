7c9555e6c1001716482e76eadb76a2156cf3f3cf
Anthony Tantra <tantra.anthony@gmail.com>
2020-05-17 04:42:41 +0800

    fixed prod build for redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,18 @@ import { createStore, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import root from "./reducers";
 
+let store;
+
 const devtools =
   process.env.NODE_ENV === "development"
     ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
       window.__REDUX_DEVTOOLS_EXTENSION__()
     : undefined;
 
-const store = createStore(root, compose(applyMiddleware(thunk), devtools));
+if (devtools) {
+  store = createStore(root, compose(applyMiddleware(thunk), devtools));
+} else {
+  store = createStore(root, compose(applyMiddleware(thunk)));
+}
 
 export default store;
